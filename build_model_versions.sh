85f5796935b3b6cfe3ed23086062be21a77c25b7
Amissum <tatsiana.kublashvili@gmail.com>
2025-12-14 20:37:05 +0100

    Updated .deploy logic to write docker images to the GitHub registry and removed unnecessary argument for build Docker images for different  model versions

--- build_model_versions.sh
+++ build_model_versions.sh
@@ -6,15 +6,11 @@ echo "Building ML gRPC Service versions..."
 
 # Version 1
 echo "Building v1.0.0..."
-docker build -t ml-hw3-grpc-service:v1.0.0 \
-  --build-arg MODEL_FILE=model.pkl \
-  -f Dockerfile .
+docker build -t ml-hw3-grpc-service:v1.0.0 -f Dockerfile .
 
 # Version 2
 echo "Building v2.0.0..."
-docker build -t ml-hw3-grpc-service:v2.0.0 \
-  --build-arg MODEL_FILE=model_v2.pkl \
-  -f Dockerfile .
+docker build -t ml-hw3-grpc-service:v2.0.0 -f Dockerfile .
 
 echo "Both versions built successfully!"
 docker images | grep ml-hw3-grpc-service
